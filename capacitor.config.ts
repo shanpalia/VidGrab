42222888bb672e5d6c4c733ff96d5b22e1911302
shanpalia <shanpalia786@gmail.com>
2026-09-12 17:57:55 +0530

    Fix Capacitor config for Codemagic build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,6 +1,4 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
+const config = {
   appId: 'com.shanpalia.vidgrab',
   appName: 'VidGrab',
   webDir: 'dist',
